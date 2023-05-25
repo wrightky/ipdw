bc0b938c7d7015b928a077b884c93fa1a2117449
wrightky <kyleawright@utexas.edu>
2023-05-25 00:20:11 -0500

    introduce main two functions

--- ipdw/interpolate.py
+++ ipdw/interpolate.py
@@ -2,3 +2,15 @@
 import numpy as np
 import matplotlib
 import skfmm
+
+def build_raster():
+    """
+    Function creates a target raster taking into account domain topology
+    """
+    return
+
+def interpolate():
+    """
+    Function performs interpolation from discete data points onto target raster
+    """
+    return
