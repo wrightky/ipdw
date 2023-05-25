54e064baba3f27e2921e3df72500bad9d97cc154
wrightky <kyleawright@utexas.edu>
2023-05-25 00:29:20 -0500

    list inputs needed

--- ipdw/interpolate.py
+++ ipdw/interpolate.py
@@ -3,13 +3,15 @@ import numpy as np
 import matplotlib
 import skfmm
 
-def build_raster():
+# Maybe this should be a class called Raster with interpolate as a method
+# used to create an ipdw.raster object
+def build_raster(extent, cellsize, boundary, holes=None):
     """
     Function creates a target raster taking into account domain topology
     """
     return
 
-def interpolate():
+def interpolate(raster, input_locations, input_values, extent, n_nearest=3):
     """
     Function performs interpolation from discete data points onto target raster
     """
