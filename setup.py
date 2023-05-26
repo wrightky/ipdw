260bc5fbc60d9329575045fe6f1fc66ca0aa0791
wrightky <kyleawright@utexas.edu>
2023-05-26 17:10:07 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name = 'ipdw',
+    version = '1.0.0',
+    license = 'MIT',
+    description = 'Inverse-Path-Distance-Weighted Interpolation for Python',
+    long_description=LONG_DESCRIPTION,
+    author = 'Kyle Wright',
+    author_email = 'Kyle.Wright@twdb.texas.gov',
+    url = 'https://github.com/wrightky/ipdw',
+    packages = find_packages(),
+    keywords='interpolation ipdw idw',
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.11',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: GIS'
+    ],
+    install_requires = ['numpy','matplotlib','scikit-fmm'],
+)
