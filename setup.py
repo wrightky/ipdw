a08d892227934385aac9f383f42dd4f7bd4a0fe4
Kyle Wright <30955855+wrightky@users.noreply.github.com>
2023-06-05 17:41:48 -0500

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name = 'ipdw',
-    version = '1.0.0',
+    version = '1.0.1',
     license = 'MIT',
     description = 'Inverse-Path-Distance-Weighted Interpolation for Python',
     long_description=LONG_DESCRIPTION,
